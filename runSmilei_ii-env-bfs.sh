3abea3af169ea87a2e6eb09093f72f461f93fbe9
Kevin Cassou <cassou@lal.in2p3.fr>
2021-03-04 12:20:20 +0100

    bash script to submit job on irene

---
+++ runSmilei_ii-env-bfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+source ~/env_skl.sh
+
+#MSUB -r smilei_mpi
+#MSUB -n 10
+#MSUB -c 24
+#MSUB -A gen10062
+#MSUB -q skylake
+#MSUB -T 3600
+#MSUB -m work,scratch
+
+export OMP_NUM_THREADS=24
+export OMP_SCHEDULE=dynamic
+export OMP_PROC_BIND=true
+
+ccc_mprun ./smilei namelist.py 
